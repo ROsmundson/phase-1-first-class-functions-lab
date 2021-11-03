ad256a4bf455611e0f6f06fba5ba8cff51221f97
Ryan Osmundson <rosmundson@outlook.com>
2021-11-03 16:08:21 -0600

    commit tree f761dbc5e5032fa081dc39a4aad1c18b2aa9920e

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fareMultiplier) {
+    return function(fare) {
+        return fare * fareMultiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+
+function selectDifferentDrivers(drivers, selectingDrivers) {
+    return selectingDrivers(drivers);
+}
+
+
+
+
+    // describe('selectDifferentDrivers(arrayOfDrivers, function)', function () {
+    //     it('returns the first two drivers when passed returnFirstTwoDrivers() as the second argument', function () {
+    //       expect(selectDifferentDrivers(drivers, returnFirstTwoDrivers)).to.eql(['Sally', 'Bob']);
+    //     });
+    
+    //     it('returns the last two drivers when passed returnLastTwoDrivers() as the second argument', function () {
+    //       expect(selectDifferentDrivers(drivers, returnLastTwoDrivers)).to.eql(['Freddy', 'Claudia']);
+    //     });
+    //   });
+    // });
